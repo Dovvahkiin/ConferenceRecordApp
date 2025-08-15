be531059d2922ac0c5d1cbc27c5887cbe0e20d44
dovvahkiin <bozidartovarnickii@gmail.com>
2025-08-15 23:31:42 +0200

    Added getRecords view

--- sql/Views/getRecords view.sql
+++ sql/Views/getRecords view.sql
@@ -1,4 +1,12 @@
-CREATE VIEW getRecords as
-SELECT r.recordID, r.recordName, rc.recordText, r.recordPreviewCount, rc.recordDate, rc.recordUpdated
-from records r
-JOIN recordcontent rc on rc.recordInfoID = r.recordID
+CREATE VIEW getRecords AS
+    SELECT 
+        r.recordID AS ID,
+        r.recordName AS RecordName,
+        rc.recordText AS RecordContent,
+        r.recordPreviewCount AS Previews,
+        rc.recordDate AS CreatedDate,
+        rc.recordUpdated AS UpdatedDate
+    FROM
+        records r
+            JOIN
+        recordcontent rc ON rc.recordInfoID = r.recordID
