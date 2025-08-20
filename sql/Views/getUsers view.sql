de53f44aad8785e5eaacc9fdaa0a165c947f60eb
dovvahkiin <bozidartovarnickii@gmail.com>
2025-08-20 12:26:30 +0200

    GetUsers fixed

--- sql/Views/getUsers view.sql
+++ sql/Views/getUsers view.sql
@@ -1,18 +1,19 @@
-Create view getusers as
-SELECT 
-    un.usernameid AS ID,
-    un.username AS UserName,
-    u.userFirstName AS FirstName,
-    u.userLastName AS LastName,
-    u.userEmail AS Email,
-    r.roleid AS RoleID,
-    r.rolename AS RoleName,
-    r.canControl,
-    r.canEdit,
-    r.canView
-FROM
-    users u
-        JOIN
-    usernames un ON un.usernameID = u.userID
-        JOIN
-    roles r ON roleID = u.whatrole;
+CREATE VIEW getusers AS
+    SELECT 
+        un.usernameid AS ID,
+        un.username AS UserName,
+        u.userFirstName AS FirstName,
+        u.userLastName AS LastName,
+        u.userEmail AS Email,
+        u.userPassword AS Password,
+        r.rolename AS RoleName,
+        r.roleid AS RoleID,
+        r.canControl,
+        r.canEdit,
+        r.canView
+    FROM
+        users u
+            JOIN
+        usernames un ON un.usernameID = u.userID
+            JOIN
+        roles r ON roleID = u.whatrole;
