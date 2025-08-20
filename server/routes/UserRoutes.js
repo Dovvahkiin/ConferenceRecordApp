7384fb6d690f9394e5a0e8f9dfb073d8745fb1de
dovvahkiin <bozidartovarnickii@gmail.com>
2025-08-20 19:25:59 +0200

    Quick fix

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -11,7 +11,7 @@ const [UserGetControl, UserPostControl] = [
   new UserPostController(),
 ];
 
-router.get("/users/:id", authToken, UserGetControl.GetUserByIdControl); // test profile
+router.get("/users/:id", authToken, UserGetControl.GetUserByIdControl); // test user profile
 router.get("/users", authToken, UserGetControl.GetUsersControl); // admin panel users authorization *admin*
 
 module.exports = router;
