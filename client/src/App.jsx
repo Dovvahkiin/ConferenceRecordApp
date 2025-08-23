f7e31116af382100a0a98efaea36a753328e6d72
dovvahkiin <bozidartovarnickii@gmail.com>
2025-08-23 03:16:38 +0200

    Small fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import "./styles/general.css";
 import router from "./routes/routes";
 import { RouterProvider, Link } from "react-router-dom";
-// map routes
 
 function App() {
   return <RouterProvider router={router} />;
