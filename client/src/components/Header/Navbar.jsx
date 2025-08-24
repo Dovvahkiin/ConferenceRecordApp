b85b14f54bd8cb6ac3c25dda2f59a9c05bd41634
dovvahkiin <bozidartovarnickii@gmail.com>
2025-08-24 19:56:21 +0200

    Fixed navigation bar bug

--- client/src/components/Header/Navbar.jsx
+++ client/src/components/Header/Navbar.jsx
@@ -4,12 +4,19 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav>
-      <Link to="/register" className="navLink">
-        Register
-      </Link>
-      <Link to="/login" className="navLink fullRight">
-        Login
-      </Link>
+      <div className="navigationLeft">
+        <Link className="navLink" to="/">
+          Home
+        </Link>
+        <Link className="navLink" to="/profile">
+          Profile
+        </Link>
+      </div>
+      <div className="navigationRight">
+        <Link to="/login" className="navLink">
+          Logout
+        </Link>
+      </div>
     </nav>
   );
 }
